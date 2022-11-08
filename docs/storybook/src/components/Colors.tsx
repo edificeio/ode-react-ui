db1af0de64e15abb51602c0a1e85b08e390094ee
varmentier <vincent.armentier@opendigitaleducation.com>
2022-11-08 09:57:57 +0100

    docs(storybook) #WB-1058 design system color

--- docs/storybook/src/components/Colors.tsx
+++ docs/storybook/src/components/Colors.tsx
@@ -1,11 +1,26 @@
-export function Colors() {
+export function Colors(props:{color: string, hexa: string, nameVar: string}) {
   return (
     <div className="ctn-card">
-      <div className="background-color orange"></div>
+      <div className="background-color" style={{backgroundColor: props.hexa}}></div>
       <div className="description-color">
-        <p className="name-color">Orange</p>
-        <p className="hexa-color">#FF8D2E</p>
+        <p className="name-color">{props.color}</p>
+        <p className="hexa-color">{props.hexa}</p>
+        <p className="variable">{props.nameVar}</p>
       </div>
     </div>
   );
 }
+
+
+// export function ColorsTab() {
+//   let infos : {
+//     color : ["Orange","blue"],
+//     hexa : ["#FE3448","#FE3448"],
+//     nameVar : ["Orange","Blue"],
+//   }
+//   return(
+//     infos.map(function(info) {
+//       console.log(info);
+//     })
+//   );
+// };
