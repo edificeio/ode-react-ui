8da2983bcec33a3d15abdbf19c94929cf5e44eab
david-cc <david.caudeli@web-education.net>
2022-12-12 22:01:52 +0100

    feat(hooks): add useKeyPress hook

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,5 +1,6 @@
 export { default as useOdeFactory } from "./useOdeFactory";
 export { default as useOdeIcons } from "./useOdeIcons";
+export { default as useKeyPress } from "./useKeyPress";
 export { default as useThemeHelper } from "./useThemeHelper";
 export * from "./useToggle";
 export * from "./useClickOutside";
