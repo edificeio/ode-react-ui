f7737ee7f1a2d5fb0720a4bf5e2d89e7f16b2b6e
david-cc <david.caudeli@web-education.net>
2022-12-14 18:46:42 +0100

    fix(treeview): #WB2-131, fix UI feedback

--- packages/advanced/src/TreeView/TreeItem.tsx
+++ packages/advanced/src/TreeView/TreeItem.tsx
@@ -15,38 +15,81 @@ export const TreeItem = (props: TreeItemProps) => {
   const { nodeId, label, children, section, selected, select } = props;
   const [expanded, setExpanded] = useState<boolean>(false);
 
-  const handleInteraction = (event: React.UIEvent<HTMLDivElement>) => {
+  const handleFoldClick = (event: React.UIEvent<HTMLDivElement>) => {
     event.preventDefault();
     setExpanded(!expanded);
+    event.stopPropagation();
+  };
+
+  const handleItemClick = (event: React.UIEvent<HTMLDivElement>) => {
+    event.preventDefault();
     select(nodeId);
     event.stopPropagation();
   };
 
+  const rafterSize = section ? 16 : 12;
+
   const renderItem = () => (
     <li id={nodeId} role="treeitem" aria-selected={selected}>
-      <div
-        onClick={handleInteraction}
-        onKeyPress={handleInteraction}
-        role="button"
-        tabIndex={0}
-        aria-expanded={expanded}
-      >
-        <div>
-          {Array.isArray(children) && !expanded && (
-            <RafterRight title="RafterRight" />
-          )}
-          {Array.isArray(children) && expanded && (
-            <RafterDown title="RafterDown" />
-          )}
-          {section && <Folder title="Folder" />}
-          <span>{label}</span>
+      <div aria-expanded={expanded}>
+        <div className="action-container d-flex align-items-center gap-8 ms-8 px-2 py-8">
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={handleFoldClick}
+            onKeyPress={handleFoldClick}
+          >
+            {Array.isArray(children) && !expanded && (
+              <RafterRight
+                title="RafterRight"
+                width={rafterSize}
+                height={rafterSize}
+              />
+            )}
+
+            {Array.isArray(children) && expanded && (
+              <RafterDown
+                title="RafterDown"
+                width={rafterSize}
+                height={rafterSize}
+              />
+            )}
+
+            {/* Hide rafter when no children to keep alignment */}
+            {!Array.isArray(children) && (
+              <RafterRight
+                title="RafterRight"
+                width={rafterSize}
+                height={rafterSize}
+                aria-hidden="true"
+                className="opacity-0"
+              />
+            )}
+          </div>
+          <div
+            tabIndex={0}
+            role="button"
+            onClick={handleItemClick}
+            onKeyPress={handleItemClick}
+            className="d-flex align-items-center gap-8"
+          >
+            {section && <Folder title="Folder" width={20} height={20} />}
+            <span>{label}</span>
+          </div>
         </div>
+
         {Array.isArray(children) && <ul role="group">{children}</ul>}
       </div>
     </li>
   );
 
-  return section ? <ul role="tree">{renderItem()}</ul> : renderItem();
+  return section ? (
+    <ul role="tree" className="m-0 p-0">
+      {renderItem()}
+    </ul>
+  ) : (
+    renderItem()
+  );
 };
 
 TreeItem.displayName = "TreeItem";
