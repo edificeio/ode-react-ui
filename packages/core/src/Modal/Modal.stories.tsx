cad584cb6d4b547cff5cee830a75de0bfd10d90b
Julien Sartory <jsartory@gmail.com>
2022-12-14 21:06:33 +0100

    feat(core): Add Modal Story Move File

--- packages/core/src/Modal/Modal.stories.tsx
+++ packages/core/src/Modal/Modal.stories.tsx
@@ -1,8 +1,12 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { useState } from "react";
-import { Button } from "../Button";
 
+import { TreeView } from "@ode-react-ui/advanced";
+import { TreeNode } from "@ode-react-ui/advanced/src/TreeView/TreeViewProps";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Button } from "../Button";
 import Modal from "./Modal";
+import ModalHeader from "./ModalHeader";
 
 export default {
   title: "Components/Core/Modal",
@@ -72,3 +76,126 @@ Primary.args = {
   scrollable: true,
 };
 Primary.decorators = [(Story) => <div className="p-12">{Story()}</div>];
+
+const data: TreeNode = {
+  id: "root",
+  name: "Section Element",
+  section: true,
+  children: [
+    {
+      id: "1",
+      name: "level 1 arborescence tree",
+      children: [
+        {
+          id: "4",
+          name: "level 2 arborescence tree",
+          children: [
+            {
+              id: "8",
+              name: "level 3 arborescence tree",
+              children: [
+                {
+                  id: "12",
+                  name: "level 4 arborescence tree",
+                },
+                {
+                  id: "13",
+                  name: "level 4 arborescence tree",
+                },
+              ],
+            },
+            {
+              id: "9",
+              name: "level 3 arborescence tree",
+            },
+          ],
+        },
+        {
+          id: "5",
+          name: "level 2 arborescence tree",
+          children: [
+            {
+              id: "10",
+              name: "level 3 arborescence tree",
+            },
+            {
+              id: "11",
+              name: "level 3 arborescence tree",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "level 1 arborescence tree",
+      children: [
+        {
+          id: "6",
+          name: "level 2 arborescence tree",
+        },
+        {
+          id: "7",
+          name: "level 2 arborescence tree",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "level 1 arborescence tree",
+    },
+  ],
+};
+const TemplateMoveFile: ComponentStory<typeof Modal> = (args) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        type="button"
+        variant="filled"
+        color="primary"
+        onClick={() => setModalOpen(true)}
+      >
+        Déplacer l'élément
+      </Button>
+      <Modal
+        {...args}
+        isOpen={isModalOpen}
+        onModalClose={() => setModalOpen(false)}
+      >
+        <Modal.Header onModalClose={() => setModalOpen(false)}>
+          Déplacer
+        </Modal.Header>
+        <Modal.Subtitle>
+          Sélectionner le dossier vers lequel déplacer les éléments
+        </Modal.Subtitle>
+        <Modal.Body>
+          <TreeView data={data} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            type="button"
+            color="primary"
+            variant="outline"
+            onClick={() => setModalOpen(false)}
+          >
+            Annuler
+          </Button>
+          <Button
+            type="button"
+            color="primary"
+            variant="filled"
+            onClick={() => setModalOpen(false)}
+          >
+            Déplacer
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+export const MoveFile = TemplateMoveFile.bind({});
+MoveFile.args = {
+  scrollable: true,
+};
