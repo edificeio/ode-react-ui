7cb561d1e05b17ceb8442c74ef89933e3364e0d7
Julien SARTORY <jsartory@gmail.com>
2022-12-28 23:07:09 +0100

    feat(form): Add Stories for input group

---
+++ packages/core/src/Form/InputGroup.stories.tsx
@@ -0,0 +1,105 @@
+import { Search } from "@ode-react-ui/icons";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useMemo, useRef, useState } from "react";
+import Button from "../Button/Button";
+
+import { FormControl, Input } from "./index";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Forms/Input Group (dev)",
+  component: FormControl,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The Input group is made with FormControl, Inputs and SearchButton components. Adding the `input-group` class to the FormControl component will add the right style. All components have access to all their expected props.",
+      },
+    },
+  },
+} as ComponentMeta<typeof FormControl>;
+
+const Template: ComponentStory<typeof FormControl> = (args) => (
+  <FormControl id={args.id} status={args.status} className="input-group">
+    <span className="input-group-text">http://ode.example.test/</span>
+    <FormControl.Input type="text" placeholder="Url path" size="md" />
+    <Button type="submit" aria-label="search">
+      Create this url
+    </Button>
+  </FormControl>
+);
+
+export const Base = Template.bind({});
+
+export const MultiInputGroup = () => {
+  const [username, setUsername] = useState<string>("");
+  const [server, setServer] = useState<string>("");
+  const serverUrl = useMemo(() => username + "@" + server, [username, server]);
+  return (
+    <div>
+      <FormControl id="connect-url" className="input-group">
+        <Input
+          type="text"
+          placeholder="Username"
+          size="lg"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <span className="input-group-text">@</span>
+        <Input
+          type="text"
+          placeholder="Server"
+          size="lg"
+          value={server}
+          onChange={(event) => setServer(event.target.value)}
+        />
+        <Button type="submit" aria-label="search">
+          Generate
+        </Button>
+      </FormControl>
+      {<div className="py-8">Your url is {serverUrl !== "@" && serverUrl}</div>}
+    </div>
+  );
+};
+
+export const CheckboxRadio = () => {
+  return (
+    <div>
+      <FormControl id="addline" className="input-group mb-12">
+        <div className="input-group-text">
+          <Input
+            type="checkbox"
+            placeholder="validate"
+            size="lg"
+            className="form-check-input mt-0"
+          />
+        </div>
+        <Input type="text" placeholder="Add this line" size="lg" />
+      </FormControl>
+      <FormControl id="slectLine" className="input-group mb-12">
+        <div className="input-group-text">
+          <Input
+            type="radio"
+            name="slectLine"
+            placeholder="validate"
+            size="lg"
+            className="form-check-input mt-0"
+          />
+        </div>
+        <Input type="text" placeholder="Select this line" size="lg" />
+      </FormControl>
+      <FormControl id="slectLine" className="input-group">
+        <div className="input-group-text">
+          <Input
+            type="radio"
+            name="slectLine"
+            placeholder="validate"
+            size="lg"
+            className="form-check-input mt-0"
+          />
+        </div>
+        <Input type="text" placeholder="Select this line" size="lg" />
+      </FormControl>
+    </div>
+  );
+};
