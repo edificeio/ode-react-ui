c2ded0b8395438de3059436b27e5dcb272ae7735
Julien SARTORY <jsartory@gmail.com>
2022-12-29 17:01:44 +0100

    feat: WB2-178 Modal Share feat: Add Checkbox component feat: Add Radio Component

--- packages/core/src/Form/index.ts
+++ packages/core/src/Form/index.ts
@@ -2,3 +2,5 @@ export { default as Input } from "./Input";
 export { default as Label } from "./Label";
 export { default as FormText } from "./FormText";
 export { default as FormControl } from "./FormControl";
+export { default as Checkbox } from "./Checkbox";
+export { default as Radio } from "./Radio";
