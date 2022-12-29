adfbc81c7e123a1eaa26feb37293b98e84f4469e
J.Sartory <jsartory@gmail.com>
2022-12-29 12:13:08 +0100

    feat(core): #WB2-122, Add ActionBar Component
    * feat(core): Add ActionBar Component
    
    * feat(actionbar): WB2-122
    
    * docs(actionbar): Add Changeset

---
+++ .changeset/orange-pets-accept.md
@@ -0,0 +1,5 @@
+---
+"@ode-react-ui/core": patch
+---
+
+Add ActionBar Component and Story
---
+++ packages/core/src/ActionBar/ActionBar.stories.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Button } from "../Button";
+import ActionBar from "./ActionBar";
+
+export default {
+  title: "Components/ActionBar",
+  component: ActionBar,
+} as ComponentMeta<typeof ActionBar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof ActionBar> = (args) => {
+  return (
+    <>
+      <ActionBar {...args} />
+    </>
+  );
+};
+
+export const Base = Template.bind({});
+Base.args = {
+  children: (
+    <>
+      <Button type="button" variant="filled" color="primary">
+        Créer
+      </Button>
+      <Button type="button" variant="filled" color="primary">
+        Modifier
+      </Button>
+      <Button type="button" variant="filled" color="primary">
+        Publier
+      </Button>
+    </>
+  ),
+};
