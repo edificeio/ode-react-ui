7fbe44c6b3a4b276907529858ad112db6a4a9fce
Julien SARTORY <jsartory@gmail.com>
2022-12-29 10:25:59 +0100

    feat(modal): Ad share modal storie

--- packages/core/src/Modal/Modal.stories.tsx
+++ packages/core/src/Modal/Modal.stories.tsx
@@ -1,12 +1,11 @@
-import { Copy, Plus } from "@ode-react-ui/icons";
+import { Copy, Plus, Search } from "@ode-react-ui/icons";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 import { Alert } from "../Alert";
-import { Button } from "../Button";
+import { Button, SearchButton } from "../Button";
 import { FormControl, Input, Label } from "../Form";
 import { Heading } from "../Heading";
 import { ImagePicker } from "../ImagePicker";
-import { TreeNode } from "../TreeView/TreeViewProps";
 import { TreeView } from "../TreeView";
 import Modal from "./Modal";
 import { data } from "../utils/mock-tree-data";
@@ -254,11 +253,13 @@ export const CreateFile = (args: any) => {
             <div className="d-flex flex-wrap align-items-center mt-16 mb-3 gap-4">
               <div>https://neoconnect.opendigitaleducation.com/</div>
 
-              <Input
-                type="text"
-                size="md"
-                placeholder="extention personnalisée"
-              />
+              <div className="flex-fill">
+                <Input
+                  type="text"
+                  size="md"
+                  placeholder="extention personnalisée"
+                />
+              </div>
               <Button
                 color="primary"
                 onClick={() => {}}
@@ -297,5 +298,138 @@ export const CreateFile = (args: any) => {
   );
 };
 CreateFile.args = {
+  scrollable: false,
+};
+
+export const ShareFile = (args: any) => {
+  const { isOpen, toggle: toggleModal } = useModal(false);
+
+  function handleOpenModal() {
+    toggleModal(true);
+  }
+
+  function handleCloseModal() {
+    toggleModal(false);
+  }
+
+  return (
+    <>
+      <Button
+        type="button"
+        variant="filled"
+        color="primary"
+        onClick={handleOpenModal}
+      >
+        Share
+      </Button>
+      <Modal
+        {...args}
+        size="lg"
+        isOpen={isOpen}
+        onModalClose={handleCloseModal}
+      >
+        <Modal.Header onModalClose={handleCloseModal}>
+          Partager avec ...
+        </Modal.Header>
+
+        <Modal.Body>
+          <Heading headingStyle="h4" level="h3" className="mb-16">
+            Liste des utilisateurs affectés par le partage
+          </Heading>
+
+          <table className="table">
+            <thead className="table-dark bg-secondary">
+              <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col">Consulter</th>
+                <th scope="col">Contribuer</th>
+                <th scope="col">Gérer</th>
+                <th scope="col">Commenter</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Image</th>
+                <td>
+                  Enseignants du groupe
+                  <br /> CLG-DENIS POISSON
+                </td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>delete</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <Heading headingStyle="h4" level="h3" className="my-24">
+            Rechercher des utilisateurs
+          </Heading>
+
+          <FormControl className="input-group" id="search">
+            <Input
+              noValidationIcon
+              placeholder="nom d’utilisateurs, groupes, favoris"
+              size="md"
+              type="search"
+            />
+            <SearchButton aria-label="search" icon={<Search />} type="submit" />
+          </FormControl>
+
+          <Heading headingStyle="h4" level="h3" className="my-24">
+            Circuit de publication des billets
+          </Heading>
+
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="publication"
+              id="publication-now"
+            />
+            <label className="form-check-label" htmlFor="publication-now">
+              Publication immédiate
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="publication"
+              id="publication-validate"
+            />
+            <label className="form-check-label" htmlFor="publication-validate">
+              Billets soumis à validation
+            </label>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            type="button"
+            color="primary"
+            variant="outline"
+            onClick={handleCloseModal}
+          >
+            Annuler
+          </Button>
+
+          <Button
+            type="button"
+            color="primary"
+            variant="filled"
+            onClick={handleCloseModal}
+          >
+            Créer
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+ShareFile.args = {
   scrollable: false,
 };
