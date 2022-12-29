951d06787179997e581f3cdbaa6d9878bf9f4d29
Julien SARTORY <jsartory@gmail.com>
2022-12-29 17:01:44 +0100

    feat: WB2-178 Modal Share feat: Add Checkbox component feat: Add Radio Component

---
+++ packages/core/src/Form/Checkbox.tsx
@@ -0,0 +1,32 @@
+import React, { forwardRef, Ref } from "react";
+
+import clsx from "clsx";
+
+export interface CheckboxProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Checkbox = forwardRef(
+  (
+    { disabled = false, checked = false, ...restProps }: CheckboxProps,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    const inputProps = {
+      ...restProps,
+      ...{
+        type: "checkbox",
+        checked,
+        disabled,
+        ref,
+        className: clsx(restProps.className, "form-check-input c-pointer"),
+      },
+    };
+
+    return (
+      <div className="position-relative d-inline-block mt-8 form-check">
+        <input {...inputProps} />
+      </div>
+    );
+  },
+);
+
+export default Checkbox;
---
+++ packages/core/src/Form/Radio.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, Ref } from "react";
+
+import clsx from "clsx";
+
+export interface RadioProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  /**
+   * State controlling radio group
+   */
+  model: string | boolean;
+  /**
+   * Label of the radio checkbox
+   */
+  label?: string | false;
+}
+
+const Radio = forwardRef(
+  (
+    {
+      model,
+      label = false,
+      disabled = false,
+      checked = false,
+      ...restProps
+    }: RadioProps,
+    ref: Ref<HTMLInputElement>,
+  ) => {
+    const inputProps = {
+      ...restProps,
+      ...{
+        type: "radio",
+        checked: model === restProps.value,
+        disabled,
+        ref,
+        className: clsx(restProps.className, "form-check-input c-pointer"),
+      },
+    };
+
+    return (
+      <div className="position-relative form-check">
+        <input {...inputProps} />
+        {label && (
+          <label className="form-check-label" htmlFor={restProps.id}>
+            {label}
+          </label>
+        )}
+      </div>
+    );
+  },
+);
+
+export default Radio;
