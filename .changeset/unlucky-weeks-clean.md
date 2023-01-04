bef68c7a723664bba7e1e69de8a49eef25ef7ffd
Clement Creusat <clement.creusat@opendigitaleducation.com>
2023-01-04 15:30:09 +0100

    fix(icons): add bookmark icon

---
+++ .changeset/unlucky-weeks-clean.md
@@ -0,0 +1,5 @@
+---
+"@ode-react-ui/icons": patch
+---
+
+add bookmark icon
---
+++ packages/icons/src/Bookmark.tsx
@@ -0,0 +1,27 @@
+import { SVGProps } from "react";
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgBookmark = ({
+  title,
+  titleId,
+  ...props
+}: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    aria-hidden="true"
+    aria-labelledby={titleId}
+    {...props}
+  >
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path
+      fill="currentColor"
+      d="M7 4a1 1 0 0 0-1 1v14.06l5.42-3.87a1 1 0 0 1 1.16 0L18 19.06V5a1 1 0 0 0-1-1H7zM4.88 2.88A3 3 0 0 1 7 2h10a3 3 0 0 1 3 3v16a1 1 0 0 1-1.58.81L12 17.23 5.58 21.8A1 1 0 0 1 4 21V5a3 3 0 0 1 .88-2.12z"
+    />
+  </svg>
+);
+export default SvgBookmark;
