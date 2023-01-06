b9b39f872919c072469454d64aef7e6b9ea9d8f4
Clement Creusat <clement.creusat@gmail.com>
2023-01-06 16:53:28 +0100

    chore(deps): update deps and update readme

--- packages/hooks/src/useToggle/useToggle.ts
+++ packages/hooks/src/useToggle/useToggle.ts
@@ -6,9 +6,8 @@ export default function useToggle(initialState = false): [boolean, any] {
   const [state, setState] = useState<boolean>(initialState);
   // Define and memorize toggler function in case we pass down the comopnent,
   // This function change the boolean value to it's opposite value
-  const toggle = useCallback(
-    (): void => setState((prevState: any) => !prevState),
-    [],
-  );
+  const toggle = useCallback((): void => {
+    setState((prevState: any) => !prevState);
+  }, []);
   return [state, toggle];
 }
