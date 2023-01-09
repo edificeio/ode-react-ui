7e6207133653a8b7e6de1c4b7bedc3829db4e4a9
Clement Creusat <clement.creusat@gmail.com>
2023-01-09 10:14:41 +0100

    fix(core): fix issue displayName for compound component

--- packages/core/src/Form/FormControl.tsx
+++ packages/core/src/Form/FormControl.tsx
@@ -51,6 +51,8 @@ const FormControl = Object.assign({}, Root, {
   Text: FormText,
 });
 
+/* This is a hack due to Storybook issue rendering displayName [Object object] */
 Root.displayName = "FormControl";
+FormControl.displayName = "FormControl";
 
 export default FormControl;
