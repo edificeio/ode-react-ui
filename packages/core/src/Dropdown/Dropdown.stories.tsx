7ad19389977a085d95ec78c4927590eebcdadc35
Julien Sartory <jsartory@gmail.com>
2023-02-06 16:22:15 +0100

    feat: Add Select List

--- packages/core/src/Dropdown/Dropdown.stories.tsx
+++ packages/core/src/Dropdown/Dropdown.stories.tsx
@@ -5,7 +5,14 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import Dropdown from "./Dropdown";
 import DropdownTrigger from "./DropdownTrigger";
 import { Button } from "../Button";
-import { Filter } from "@ode-react-ui/icons";
+import {
+  Filter,
+  SortAscendingLetters,
+  SortDescending,
+  SortDescendingLetters,
+} from "@ode-react-ui/icons";
+import SelectList from "./SelectList";
+import { OptionListItemType } from "./SelectListProps";
 
 export default {
   title: "Components/Dropdown",
@@ -14,33 +21,66 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Dropdown> = (args: any) => {
+  const [listModel, setListModel] = useState<(string | number)[]>([]);
+
+  const options: OptionListItemType[] = [
+    {
+      value: 1,
+      label: "Item choice",
+      icon: SortAscendingLetters,
+    },
+    {
+      value: 2,
+      label: "Item choice 2",
+      icon: SortDescendingLetters,
+    },
+    {
+      value: 3,
+      label: "Item choice 3",
+      icon: SortDescending,
+    },
+  ];
+
   return (
-    <>
-      <Dropdown {...args} />
-    </>
+    <div>
+      <div className="d-flex justify-content-between ">
+        <Dropdown
+          trigger={
+            <DropdownTrigger
+              title="Dropdown toggle"
+              icon={<Filter width={20} />}
+              badgeContent={listModel.length > 0 ? listModel.length : undefined}
+            />
+          }
+          content={
+            <SelectList
+              options={options}
+              model={listModel}
+              onChange={(model) => setListModel(model)}
+            />
+          }
+        />
+
+        <Dropdown
+          trigger={
+            <DropdownTrigger
+              title="Dropdown toggle"
+              icon={<Filter width={20} />}
+            />
+          }
+          content={
+            <SelectList
+              options={options}
+              model={listModel}
+              onChange={(model) => setListModel(model)}
+              hideCheckbox
+            />
+          }
+        />
+      </div>
+      <div style={{ height: "200px" }}></div>
+    </div>
   );
 };
 
 export const Base = Template.bind({});
-Base.args = {
-  trigger: (
-    <DropdownTrigger title="Dropdown toggle" icon={<Filter width={20} />} />
-  ),
-  content: (
-    <ul className="dropdown-menu show">
-      <li>
-        <a className="dropdown-item" href="#">
-          Action
-        </a>
-      </li>
-      <li>
-        <hr className="dropdown-divider" />
-      </li>
-      <li>
-        <a className="dropdown-item" href="#">
-          Separated link
-        </a>
-      </li>
-    </ul>
-  ),
-};
---
+++ packages/core/src/Dropdown/SelectList.tsx
@@ -0,0 +1,87 @@
+/**
+ * SelectList Component
+ *
+ * @see Docs     https://ode-react-ui.vercel.app/?path=/docs/components-core-selectList--base
+ * @see Source   https://github.com/opendigitaleducation/ode-react-ui/blob/main/packages/core/src/Dropdown/SelectList.tsx
+ */
+
+import React, { useEffect, useState } from "react";
+
+import clsx from "clsx";
+
+import { OptionListItemType, SelectListProps } from "./SelectListProps";
+
+const SelectList = ({
+  options,
+  hideCheckbox,
+  onChange = (model) => {},
+  model = [],
+}: SelectListProps) => {
+  const [localModel, setLocalModel] = useState(model);
+  const handleOptionClick = (value: string | number) => {
+    setLocalModel((previousModel) => {
+      const newModel = [...previousModel];
+      if (previousModel.includes(value)) {
+        newModel.splice(newModel.indexOf(value), 1);
+      } else {
+        newModel.push(value);
+      }
+      return newModel;
+    });
+  };
+
+  useEffect(() => {
+    onChange(localModel);
+  }, [localModel]);
+
+  return (
+    <>
+      <div className="select-list">
+        {options?.map((option: OptionListItemType, index: number) => (
+          <div
+            key={option.value}
+            className={clsx(
+              "select-list-option",
+              hideCheckbox &&
+                localModel.includes(option.value) &&
+                "text-secondary fw-bold",
+            )}
+            role="checkbox"
+            aria-checked={localModel.includes(option.value)}
+            onMouseUp={() => {
+              handleOptionClick(option.value);
+            }}
+            tabIndex={0}
+            onKeyDown={(event) => {
+              console.log(event);
+              event.key === "Enter" && handleOptionClick(option.value);
+            }}
+          >
+            <div className="select-list-option-icon">
+              {option.icon &&
+                React.createElement(option.icon, {
+                  width: "20px",
+                  height: "20px",
+                })}
+            </div>
+            <div className="select-list-option-label">{option.label}</div>
+            {!hideCheckbox && (
+              <div className="select-list-option-checkbox">
+                <input
+                  readOnly
+                  type="checkbox"
+                  tabIndex={-1}
+                  checked={localModel.includes(option.value)}
+                  className="form-check-input c-pointer"
+                  id="option.value"
+                />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+SelectList.displayName = "SelectList";
+export default SelectList;
