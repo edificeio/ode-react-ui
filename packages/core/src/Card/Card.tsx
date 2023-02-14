074878e07cc301585150bed71e9dab0ca8e09edf
varmentier <vincent.armentier@opendigitaleducation.com>
2023-02-14 18:00:03 +0100

    fix(core): fix issues with appicons

--- packages/core/src/Card/Card.tsx
+++ packages/core/src/Card/Card.tsx
@@ -71,7 +71,7 @@ const Card = forwardRef(
       },
     );
 
-    const appCode = getIconCode(app);
+    const appCode = app ? getIconCode(app) : "";
 
     const classesFiles = clsx(`color-app-${appCode}`, {
       placeholder: isLoading,
