07816173cef97db76ffa349366729a25fbd3261d
Clement Creusat <clement.creusat@opendigitaleducation.com>
2023-02-14 16:44:18 +0100

    feat(core): add loadingscreen component

---
+++ packages/core/src/LoadingScreen/LoadingScreen.tsx
@@ -0,0 +1,33 @@
+/**
+ * LoadingScreen Component  Component
+ *
+ * @see Docs     https://ode-react-ui.vercel.app/?path=/docs/components-core-alert--base
+ * @see Source   https://github.com/opendigitaleducation/ode-react-ui/blob/main/packages/core/src/LoadingScreen/LoadingScreen.tsx
+ */
+
+import { forwardRef, Ref } from "react";
+
+import clsx from "clsx";
+
+const LoadingScreen = forwardRef((props, ref: Ref<HTMLDivElement>) => {
+  const image = "/assets/themes/ode-bootstrap/images/screen-loading.gif";
+
+  const classes = clsx(
+    "loading-screen position-fixed top-0 end-0 start-0 bottom-0 d-grid justify-content-center align-items-center z-2000",
+  );
+
+  return (
+    <>
+      <div ref={ref} className={classes}>
+        <div className="bg-white rounded-circle mx-auto">
+          <img src={image} alt="loading" />
+        </div>
+      </div>
+      <div className="modal-backdrop fade show"></div>
+    </>
+  );
+});
+
+LoadingScreen.displayName = "LoadingScreen";
+
+export default LoadingScreen;
