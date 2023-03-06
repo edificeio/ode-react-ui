8f67ba5926f38152bbcc8a01799ecf31a4756067
Clement Creusat <clement.creusat@gmail.com>
2023-03-06 09:47:18 +0100

    feat(core): add visually-hidden component

---
+++ packages/core/src/VisuallyHidden/VisuallyHidden.tsx
@@ -0,0 +1,14 @@
+import { forwardRef, ReactNode, Ref } from "react";
+
+const VisuallyHidden = forwardRef(
+  ({ children }: { children: ReactNode }, ref: Ref<HTMLSpanElement>) => {
+    return (
+      <span className="visually-hidden" ref={ref}>
+        {children}
+      </span>
+    );
+  },
+);
+
+VisuallyHidden.displayName = "VisuallyHidden";
+export default VisuallyHidden;
