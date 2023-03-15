4144342b3ac74b5fc88315bc79b5cd94194d5a8e
david-cc <david.caudeli@web-education.net>
2023-03-15 15:17:26 +0100

    feat: #WB2-404, add the possibility to have only one element selected in Select List Component

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,6 +5,7 @@ export * from "./AppIcon";
 export * from "./Avatar";
 export * from "./Button";
 export * from "./Card";
+export * from "./Dropdown";
 export * from "./EmptyScreen";
 export * from "./Form";
 export * from "./Grid";
