7277e7a1f9e405ddc961074837917c76fb8e659f
Clement Creusat <clement.creusat@gmail.com>
2023-06-01 17:38:25 +0200

    fix: add restprops to layout component to pass additional props

--- packages/components/src/Layout/Layout.tsx
+++ packages/components/src/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { ComponentPropsWithoutRef, type ReactNode } from "react";
 
 import { Main } from "@ode-react-ui/components";
 import { useTheme } from "@ode-react-ui/core";
@@ -6,6 +6,10 @@ import clsx from "clsx";
 import { Toaster } from "react-hot-toast";
 
 import { Header } from "../Header";
+
+export interface LayoutProps extends ComponentPropsWithoutRef<any> {
+  children: ReactNode;
+}
 
 const Layout = ({ children, ...restProps }: { children: ReactNode }) => {
   const { is1d, basePath } = useTheme();
